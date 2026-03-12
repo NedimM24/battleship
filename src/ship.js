7203ccfe35c2a0110c10cd220881ee3478c1d183
Nedim <nedim_mulahusic@yahoo.com>
2026-03-12 12:06:01 -0700

    changed isSunk variable to sunk
    isSunk will be reserved for a member function

--- src/ship.js
+++ src/ship.js
@@ -2,14 +2,17 @@ export class Ship{
     constructor(length){
         this.length = length;
         this.timesHit = 0;
-        this.isSunk = false;
+        this.sunk = false;
     };
 
     // function that increases the number of ‘hits’ in your ship.
     hit(){
-        if(this.isSunk === true){
+        if(this.sunk === true){
             return;
         }
         this.timesHit++;
     }
+
+
+    
 };
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,11 +1,11 @@
 import { Ship } from "../ship.js";
 
 //test creating an object from the ship class
-test('Creates a Ship object with a length of n, timesHit of 0, and isSunk = false', () => {
+test('Creates a Ship object with a length of n, timesHit of 0, and sunk = false', () => {
     const testShip = new Ship(5);
     expect(testShip.length).toBe(5);
     expect(testShip.timesHit).toBe(0);
-    expect(testShip.isSunk).toBe(false);
+    expect(testShip.sunk).toBe(false);
 });
 
 //testing that timesHit will incerase by one once hit() function is called

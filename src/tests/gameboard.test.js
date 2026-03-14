be948f3a14e9f72c0bcf7cc4c4c816d9fd855017
Nedim <nedim_mulahusic@yahoo.com>
2026-03-14 12:18:13 -0700

    created bounds tests
    tests whether vertical and horizontal ships are in bounds or not

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,6 +1,7 @@
 import { Ship } from "../ship.js";
 import { Gameboard } from "../gameboard.js";
 
+//Create a gameboard object
 test('Create a Gameboard obj', () => {
     const newGameboard = new Gameboard(3,3);
     expect(newGameboard.board).toEqual([
@@ -9,3 +10,29 @@ test('Create a Gameboard obj', () => {
         [null,null,null]
     ]);
 });
+
+let newGameboard;
+beforeEach(() => {
+    newGameboard = new Gameboard(4,4);
+});
+
+
+test("Vertical ship out of bounds returns false", () => {
+    const testShip = new Ship(2);
+    expect(newGameboard.placeShip(3, 3, 'vertical', testShip)).toBe(false);
+})
+
+test("Vertical ship in bounds returns true", () => {
+    const testShip = new Ship(2);
+    expect(newGameboard.placeShip(1, 1, 'vertical', testShip)).toBe(true);
+})
+
+test("Horizontal ship out of bounds returns false", () => {
+    const testShip = new Ship(2);
+    expect(newGameboard.placeShip(3, 3, 'horizontal', testShip)).toBe(false);
+})
+
+test("Horizontal ship in bounds returns true", () => {
+    const testShip = new Ship(2);
+    expect(newGameboard.placeShip(1, 1, 'horizontal', testShip)).toBe(true);
+})

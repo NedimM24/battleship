476091ed9826a52987ba77379bf98c2be62724b4
Nedim <nedim_mulahusic@yahoo.com>
2026-03-14 11:50:58 -0700

    wrote a test to check gameboard creation
    Will pass if board matches passed gameboard size

--- src/gameboard.js
+++ src/gameboard.js
@@ -20,9 +20,51 @@ export class Gameboard{
 
     //Gameboards should be able to place 
     //ships at specific coordinates by calling the ship factory or class.
+    placeShip(x, y, direction, ship){
+        const length = ship.length;
+        //Checking bounds
+        if(direction === 'vertical' && x + length > this.rows) {
+            return false;
+        }
+        if(direction === 'horizontal' && y + length > this.columns){
+            return false;
+        }
 
-    placeShip(x,y){
+        //checking for ship overlap
+        for(let i = 0; i < length; i++){
+            let row;
+            let col;
 
-    }
+            if(direction === 'vertical'){
+                row = x + i;
+                col = y;
+            } else {
+                row = x;
+                col = y + i;
+            }
 
-}
+            if(this.board[row][col] !== null){
+                return false;
+            }
+        }
+
+        //Place the ship
+        // Iterate through the ships length
+        for(let i = 0; i < length; i++){
+            let row;
+            let col;
+
+            if(direction === 'vertical'){
+                row = x + i;
+                col = y;
+                //HOIZONTAL
+            } else {
+                row = x;
+                col = y + i;
+              }
+             this.board[row][col] = ship;
+           }
+         return true;
+
+        }
+    };
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,3 +1,11 @@
 import { Ship } from "../ship.js";
 import { Gameboard } from "../gameboard.js";
 
+test('Create a Gameboard obj', () => {
+    const newGameboard = new Gameboard(3,3);
+    expect(newGameboard.board).toEqual([
+        [null, null, null], 
+        [null, null, null], 
+        [null,null,null]
+    ]);
+});

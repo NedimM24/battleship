65ee5031582ab695de38caf211c7dd8ad137ab6b
Nedim <nedim_mulahusic@yahoo.com>
2026-03-18 11:12:34 -0700

    Modified render function
    Added cell positions to make it possible to place ships

--- src/view.js
+++ src/view.js
@@ -5,6 +5,14 @@ export function render(){
     for(let i = 0; i <100; i++){
         let playerCell = document.createElement('div');
         playerCell.classList.add('player-cell');
+
+        //Need to add cell positions
+        const row = Math.floor(i / 10);
+        const col = i % 10;
+
+        playerCell.dataset.row = row;
+        playerCell.dataset.col = col;
+
         playerBoard.appendChild(playerCell)
         };
 
@@ -13,6 +21,16 @@ export function render(){
     for(let i = 0; i <100; i++){
         let computerCell = document.createElement('div');
         computerCell.classList.add('computer-cell');
+
+        //Need to add cell positions
+        const row = Math.floor(i / 10);
+        const col = i % 10;
+
+        computerCell.dataset.row = row;
+        computerCell.dataset.col = col;
+
         computerBoard.appendChild(computerCell)
         };
+
+    
 }
